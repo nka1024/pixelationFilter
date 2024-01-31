f4a0798c56c14fd1ef86fb4445236be529e27b87
Kirill Nepomnyaschiy <nka1024@gmail.com>
2024-01-31 15:52:09 +0700

    init

---
+++ Assets/MoveX.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveX : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.right * _speed;
+    }
+}
